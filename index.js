4ebb49ef5fa9860bb10f7d482d3930a1dcc32f06
don <donalduck254@gmail.com>
2025-12-10 06:44:31 +0300

    changes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  for (let arr of args) {
+    combinedObject.users.push(...args)
+  }
+  combinedObject.merge_date = Date.today().toString('d/M/yyyy');
 
-
+  return combinedObject
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })

2c248300e3d2c5bd466bbdd26b37ccde28f0783b
don <donalduck254@gmail.com>
2025-12-15 12:19:25 +0300

    changes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// importing datejs
 var require = ("datejs")
 
 function combineUsers(...args) {
+  // the return object
   const combinedObject = {
     users: []
   }
+
+  // looping all arrays and merging 
   args.forEach(array => {
     combinedObject.users.push(...array)
   })
-
-  combinedObject.merge_date = Date.today().toString('d/M/yyyy');
+//add todayss date in format M/d/yyy
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
 
   return combinedObject
 }
